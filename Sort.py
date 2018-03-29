02a929c81cbad2e05fc5f67df56888e75debfa5b
neos <akatsuki10086@gmail.com>
2018-03-29 18:30:47 +0800

    Update Sort.py

--- Sort.py
+++ Sort.py
@@ -8,3 +8,13 @@ def GnomeSort(li):
             i-=1
         else:
             i+=1
+
+# 插入排序
+def InsertSort(li):
+    length=len(li)
+    for i in range(1,length):
+        tmp=li[i]
+        while i and tmp<li[i-1]:
+            li[i]=li[i-1]
+            i-=1              
+        li[i]=tmp
