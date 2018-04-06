8bbea0e571506a5c0ff4b99212449ec58e0d4997
neos <akatsuki10086@gmail.com>
2018-04-07 00:10:56 +0800

    Update UnionFindSet.py

--- UnionFindSet.py
+++ UnionFindSet.py
@@ -1,12 +1,14 @@
 # 并查集
 class UnionFindSet:
-    def __init__(self,sets=[-4,0,1,1,-6,4,4,5,5,5]): #非负数指向父节点的下标,负数n代表该根节点下有|n|个孩子
+    def __init__(self,sets=[-4,0,1,1,-6,4,4,5,7,5]): #非负数指向父节点的下标,负数n代表该根节点下有|n|个孩子
         '''
              A       E
              |      / \            A B C D E F G H I J
              B     F   G           | | | | | | | | | |
-            / \   /|\              0 1 2 3 4 5 6 7 8 9
-           C   D  HIJ
+            / \   / \              0 1 2 3 4 5 6 7 8 9
+           C   D  H  J
+                  |
+                  I
         '''
         self.__set=sets
 
@@ -28,3 +30,13 @@ class UnionFindSet:
             self.__set[i]=self.find(self.__set[i])
             return self.__set[i]
         return i
+    
+    # def find(self,i):  #迭代版路径压缩算法(查找父亲的同时减小树的深度)
+    #     j=i
+    #     while self.__set[j]>=0:
+    #         j=self.__set[j]
+    #     while self.__set[i]>=0:
+    #         k=self.__set[i]
+    #         self.__set[i]=j
+    #         i=k
+    #     return j
