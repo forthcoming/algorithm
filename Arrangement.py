dcbfa6eaa61c70802f6991f8b0b60c5d76c9dad6
neos <akatsuki10086@gmail.com>
2018-04-17 23:14:03 +0800

    Update Arrangement.py

--- Arrangement.py
+++ Arrangement.py
@@ -1,1 +1,13 @@
 # 排列
+def arrangement(s,left,right):
+    if left==right:
+        print(s)
+    else:
+        for j in range(left,right+1):
+            for k in range(left,j):   #去重
+                if s[k]==s[j]:
+                    break
+            else: 
+                s[j],s[left]=s[left],s[j]
+                arrangement(s,left+1,right)
+                s[j],s[left]=s[left],s[j]   #注意此处要还原
