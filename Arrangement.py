ba3c5335b4706531ee9426ae7ec2239235c0eacd
neos <akatsuki10086@gmail.com>
2018-04-18 21:57:44 +0800

    Update Arrangement.py

--- Arrangement.py
+++ Arrangement.py
@@ -11,3 +11,20 @@ def arrangement(s,left,right):
                 s[j],s[left]=s[left],s[j]
                 arrangement(s,left+1,right)
                 s[j],s[left]=s[left],s[j]   #注意此处要还原
+                
+def nextPermutations(s):  #不去重
+    length=len(s)
+    arr=list(range(length))
+    while True:
+        print(''.join(s[i] for i in arr))
+        for i in range(length-2,-1,-1):
+            if arr[i]<arr[i+1]:
+                break
+        else:
+            break
+
+        for j in range(length-1,i,-1):
+            if arr[i]<arr[j]:
+                arr[i],arr[j]=arr[j],arr[i]
+                break
+        arr[i+1:]=arr[:i:-1]
