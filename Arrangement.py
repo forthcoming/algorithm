4d69878a00ece33cca23a72dd9723d52c3cc838b
neos <akatsuki10086@gmail.com>
2018-04-18 22:27:01 +0800

    Update Arrangement.py

--- Arrangement.py
+++ Arrangement.py
@@ -18,20 +18,20 @@ def arrangement(s,left,right):
 找到排列中最后(最右)一个升序的首位位置i,x = ai
 找到排列中第i位右边最后一个比ai大的位置j,y = aj
 交换x,y
+prePermutation只需要将Ⅰ逆序并将Ⅱ,Ⅲ处<变为>即可
 '''
-def nextPermutations(s):  #不去重
+def nextPermutation(s):  #不去重
     length=len(s)
-    arr=list(range(length))
+    arr=list(range(length))  # Ⅰ
     while True:
         print(''.join(s[i] for i in arr))
         for i in range(length-2,-1,-1):
-            if arr[i]<arr[i+1]:
+            if arr[i]<arr[i+1]:   # Ⅱ
                 break
         else:
             break
-
         for j in range(length-1,i,-1):
-            if arr[i]<arr[j]:
+            if arr[i]<arr[j]:  # Ⅲ
                 arr[i],arr[j]=arr[j],arr[i]
                 break
         arr[i+1:]=arr[:i:-1]
