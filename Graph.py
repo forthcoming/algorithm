8cab7803bdd84a80c12e79accbd1edd5f92bc61b
neos <akatsuki10086@gmail.com>
2018-06-05 11:10:12 +0800

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -36,37 +36,38 @@ class Graph:  #邻接表存储
                 self.__vertices[to]=Edge(come,weight,self.__vertices[to])
             else:
                 self.__vertices[to]=Edge(come,weight)
-                
-      def delete(self,come,to):
-        if (come in self.__vertices) and (to in self.__vertices):
-            self.__edge_num-=1
     
+    def delete(self,come,to):
+        if (come in self.__vertices) and (to in self.__vertices):    
             edge=self.__vertices[come]
-            if edge.vertex==to:
-                self.__vertices[come]=edge.right
-            else:
-                pre=edge
-                edge=edge.right
-                while edge:
-                    if edge.vertex==to:
-                        pre.right=edge.right
-                        break
-                    pre=edge
-                    edge=edge.right
-    
-            if self.__kind=='UDG': # undirected graph
-                edge=self.__vertices[to]
-                if edge.vertex==come:
-                    self.__vertices[to]=edge.right
+            if edge:
+                self.__edge_num-=1
+                if edge.vertex==to:
+                    self.__vertices[come]=edge.right
                 else:
                     pre=edge
                     edge=edge.right
                     while edge:
-                        if edge.vertex==come:
+                        if edge.vertex==to:
                             pre.right=edge.right
                             break
                         pre=edge
                         edge=edge.right
+        
+            if self.__kind=='UDG': # undirected graph
+                edge=self.__vertices[to]
+                if edge:
+                    if edge.vertex==come:
+                        self.__vertices[to]=edge.right
+                    else:
+                        pre=edge
+                        edge=edge.right
+                        while edge:
+                            if edge.vertex==come:
+                                pre.right=edge.right
+                                break
+                            pre=edge
+                            edge=edge.right
                         
     def _DFS(self,vertex,vertices=set()): # don't assign to vertices 
         if vertex not in vertices:
