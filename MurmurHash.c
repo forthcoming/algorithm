20198cc99453cfd0b3dc5c7196df76481842b73f
neos <akatsuki10086@gmail.com>
2018-07-06 23:11:00 +0800

    Update MurmurHash.c

--- MurmurHash.c
+++ MurmurHash.c
@@ -5,9 +5,7 @@ uint64_t MurmurHash64A ( const void * key, int len, unsigned int seed ) // 64-bi
 {
         const uint64_t m = 0xc6a4a7935bd1e995;
         const int r = 47;
-
         uint64_t h = seed ^ (len * m);
-
         const uint64_t * data = (const uint64_t *)key;  //  const int* a  表示*a是常量
         const uint64_t * end = data + (len>>3);         //  int* const a  表示a是常量
 
@@ -22,22 +20,27 @@ uint64_t MurmurHash64A ( const void * key, int len, unsigned int seed ) // 64-bi
         }
         const unsigned char * data2 = (const unsigned char*)data;
 
-        switch(len & 7)
-        {
-        case 7: h ^= (uint64_t)data2[6] << 48;
-        case 6: h ^= (uint64_t)data2[5] << 40;
-        case 5: h ^= (uint64_t)data2[4] << 32;
-        case 4: h ^= (uint64_t)data2[3] << 24;
-        case 3: h ^= (uint64_t)data2[2] << 16;
-        case 2: h ^= (uint64_t)data2[1] << 8;
-        case 1: h ^= (uint64_t)data2[0];
+        switch(len & 7){  // pay attention,no break here
+            case 7: 
+                h ^= (uint64_t)data2[6] << 48;
+            case 6: 
+                h ^= (uint64_t)data2[5] << 40;
+            case 5: 
+                h ^= (uint64_t)data2[4] << 32;
+            case 4: 
+                h ^= (uint64_t)data2[3] << 24;
+            case 3: 
+                h ^= (uint64_t)data2[2] << 16;
+            case 2: 
+                h ^= (uint64_t)data2[1] << 8;
+            case 1: 
+                h ^= (uint64_t)data2[0];
                 h *= m;
         };
 
         h ^= h >> r;
         h *= m;
         h ^= h >> r;
-
         return h;
 }
 
