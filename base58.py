04a18934fd5e94024edfd74f8d2d3f57f004e81c
neos <akatsuki10086@gmail.com>
2018-09-16 15:19:08 +0800

    Update base58.py

--- base58.py
+++ base58.py
@@ -7,6 +7,16 @@ from hashlib import sha256
 import binascii
 
 alphabet = b'123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+mapping = [
+    -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
+    -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
+    -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
+    -1, 0, 1, 2, 3, 4, 5, 6,  7, 8,-1,-1,-1,-1,-1,-1,
+    -1, 9,10,11,12,13,14,15, 16,-1,17,18,19,20,21,-1,
+    22,23,24,25,26,27,28,29, 30,31,32,-1,-1,-1,-1,-1,
+    -1,33,34,35,36,37,38,39, 40,41,42,43,-1,44,45,46,
+    47,48,49,50,51,52,53,54, 55,56,57,-1,-1,-1,-1,-1,
+]
 sha=lambda message:sha256(sha256(message).digest()).digest()
 
 def encode_base58(v):
@@ -29,7 +39,7 @@ def decode_base58(v):
     v = v.lstrip(b'1')
     acc = 0
     for char in v:
-        acc = acc * 58 + alphabet.index(char)
+        acc = acc * 58 + mapping[char]
     result = bytearray()
     while acc:
         acc, mod = divmod(acc, 256)
