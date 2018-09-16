7abc81b1d897c9a4dd3bab079e9bd9e9c0421242
neos <akatsuki10086@gmail.com>
2018-09-16 12:22:29 +0800

    Update base58.py

--- base58.py
+++ base58.py
@@ -5,22 +5,21 @@ import binascii
 
 alphabet = b'123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
-def b58encode(v):
-    v = v.encode('utf8')
-    zeros = len(v)
+def encode_base58(v):
+    if isinstance(v, str):
+        v = v.encode('utf8')
+    origlen = len(v)    
     v = v.lstrip(b'\0') # Skip & count leading zeroes.
-    zeros -= len(v)
     acc = int(binascii.hexlify(v),16) # if v=b'ab' then acc=0b0110000101100010
     string = b""
     while acc:
         acc, idx = divmod(acc, 58)
         string = alphabet[idx:idx+1] + string
-    return alphabet[0:1] * zeros + string
+    return alphabet[0:1] * (origlen-len(v)) + string
 
-def b58decode(v):
+def decode_base58(v):
     origlen = len(v)
     v = v.lstrip(alphabet[0:1])
-    newlen = len(v)
     acc = 0
     for char in v:
         acc = acc * 58 + alphabet.index(char)
@@ -28,22 +27,25 @@ def b58decode(v):
     while acc > 0:
         acc, mod = divmod(acc, 256)
         result.append(mod)
-    return (b'\0' * (origlen - newlen) + bytes(reversed(result)))
+    return (b'\0' * (origlen - len(v)) + bytes(result[::-1]))
 
-def b58encode_check(v):
-    '''Encode a string using Base58 with a 4 character checksum'''
+def encode_check(v):
+    if isinstance(v, str):
+        v = v.encode('utf8')
     digest = sha256(sha256(v).digest()).digest()
-    return b58encode(v + digest[:4])
+    return encode_base58(v + digest[:4])
 
-def b58decode_check(v):
-    '''Decode and verify the checksum of a Base58 encoded string'''
-    result = b58decode(v)
-    result, check = result[:-4], result[-4:]
+def decode_check(v):
+    v = decode_base58(v)
+    result, check = v[:-4], v[-4:]
     digest = sha256(sha256(result).digest()).digest()
     if check != digest[:4]:
         raise ValueError("Invalid checksum")
     return result
 
 if __name__ == '__main__':
-    print(b58encode('hello world'))
-    print(b58decode(b'StV1DL6CwTryKyV'))
+    print(encode_base58('hello world'))            # b'StV1DL6CwTryKyV'
+    print(decode_base58(b'StV1DL6CwTryKyV'))       # b'hello world'
+    print(encode_check('hello world'))             # b'3vQB7B6MrGQZaxCuFg4oh'
+    print(decode_check(b'3vQB7B6MrGQZaxCuFg4oh'))  # b'hello world'
+    # decode_check(b'4vQB7B6MrGQZaxCuFg4oh')       # ValueError: Invalid checksum
