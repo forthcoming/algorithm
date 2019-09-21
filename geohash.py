71d63644919c69de34b0d1be54cc080d5cf1bfb9
neos <akatsuki10086@gmail.com>
2019-09-21 14:46:34 +0800

    Update geohash.py

--- geohash.py
+++ geohash.py
@@ -46,3 +46,87 @@ def geo_decode(geohash):
                     lat_interval = (lat_interval[0], (lat_interval[0]+lat_interval[1])/2)
             even = not even
     return (lon_interval[0] + lon_interval[1]) / 2,(lat_interval[0] + lat_interval[1]) / 2
+
+
+class GEO:
+    _earth_radius = 6372797.560856
+
+    def __init__(self,step=30):
+        assert 0<step<=32
+        self.scale=1<<step
+
+    def encode(self,longitude,latitude):
+        assert -180<=longitude<=180 and -90<=latitude<=90
+        lon_offset = longitude/360 + .5
+        lat_offset = latitude/180 + .5
+        lon_offset *= self.scale
+        lat_offset *= self.scale
+        return GEO.interleave64(int(lat_offset),int(lon_offset))
+        # return __class__.interleave64(int(lat_offset),int(lon_offset))  # py3
+
+    def decode(self,geohash):
+        # lon_offset,lat_offset = __class__.deinterleave64(geohash)
+        lon_offset,lat_offset = GEO.deinterleave64(geohash)
+        longitude = 360*lon_offset/self.scale-180
+        latitude = 180*lat_offset/self.scale-90
+        return longitude,latitude 
+
+    @staticmethod
+    def distance(lon1, lat1, lon2, lat2):
+        percent = math.pi / 180
+        lon1 *= percent
+        lat1 *= percent
+        lon2 *= percent
+        lat2 *= percent
+        u = math.sin((lat2 - lat1) / 2)
+        v = math.sin((lon2 - lon1) / 2)
+        # return round(2 * __class__._earth_radius * math.asin((u * u + math.cos(lat1) * math.cos(lat2) * v * v)**.5))
+        return round(2 * GEO._earth_radius * math.asin((u * u + math.cos(lat1) * math.cos(lat2) * v * v)**.5))
+
+    @staticmethod
+    def interleave64(x, y):
+        B = [0x5555555555555555, 0x3333333333333333,0x0F0F0F0F0F0F0F0F, 0x00FF00FF00FF00FF,0x0000FFFF0000FFFF]
+        S = [1, 2, 4, 8, 16]
+
+        x = (x | (x << S[4])) & B[4]  
+        y = (y | (y << S[4])) & B[4] 
+    
+        x = (x | (x << S[3])) & B[3] 
+        y = (y | (y << S[3])) & B[3] 
+    
+        x = (x | (x << S[2])) & B[2] 
+        y = (y | (y << S[2])) & B[2] 
+    
+        x = (x | (x << S[1])) & B[1] 
+        y = (y | (y << S[1])) & B[1] 
+    
+        x = (x | (x << S[0])) & B[0] 
+        y = (y | (y << S[0])) & B[0] 
+    
+        return x | (y << 1) 
+
+    @staticmethod
+    def deinterleave64(interleaved):
+        B = [0x3333333333333333,0x0F0F0F0F0F0F0F0F, 0x00FF00FF00FF00FF,0x0000FFFF0000FFFF, 0x00000000FFFFFFFF]
+        S = [1, 2, 4, 8, 16]
+
+        x = interleaved & 0x5555555555555555
+        y = (interleaved >> 1) & 0x5555555555555555
+    
+        x = (x | (x >> S[0])) & B[0]
+        y = (y | (y >> S[0])) & B[0]
+    
+        x = (x | (x >> S[1])) & B[1]
+        y = (y | (y >> S[1])) & B[1]
+    
+        x = (x | (x >> S[2])) & B[2]
+        y = (y | (y >> S[2])) & B[2]
+    
+        x = (x | (x >> S[3])) & B[3]
+        y = (y | (y >> S[3])) & B[3]
+    
+        x = (x | (x >> S[4])) & B[4]
+        y = (y | (y >> S[4])) & B[4]
+        
+        return y, x
+ 
