1f59b8ad942535dc9a7f5216848b995f667e8baa
neos <akatsuki10086@gmail.com>
2023-05-24 20:42:58 +0800

    update

--- base32.py
+++ base32.py
@@ -1,34 +1,62 @@
-'''
+"""
 base64是一种基于64个可打印字符来表示二进制数据的表示方法,每6个比特为一个单元,对应某个可打印字符,编码后数据是原先的1.333倍
 base32是一种基于32个可打印字符来表示二进制数据的表示方法,每5个比特为一个单元,对应某个可打印字符,编码后数据是原先的1.6倍
 baseN不是加密,主要用途是把一些二进制数转成普通字符用于网络传输,另外还有一些系统中只能使用ASCII字符,BaseN就是用来将非ASCII字符数据转换成ASCII字符
 url编码一般使用base32,应为其字母不区分大小写
 base32要求结果串长度必须是8的倍数,所以目标串必须是5的倍数
-'''
-alphabet=b"ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
+"""
+import base64
+
+alphabet = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
 _b32tab2 = None
 
+
 def encode_base32(message):  # base64.b32encode(b'abcd')
-    length=len(message)
-    idx=0
-    buffer=0
-    left=0
-    result=bytearray()  
-    while left>5 or idx<length:
-        if left<5:
-            buffer=0b1111111111111111 & ((buffer<<8) | message[idx])  # 最多只有12个有效bit位,b字符串按下标取出时会转换为int类型
-            left+=8
-            idx+=1
-        result.append(alphabet[0b11111 & (buffer>>(left-5))])
-        left-=5
-    if left: # 防止left还有剩余
-        result.append(alphabet[0b11111 & (buffer<<(5-left))])
-    pad=len(result)%8
+    length = len(message)
+    idx = 0
+    buffer = 0
+    left = 0
+    result = bytearray()
+    while left > 5 or idx < length:
+        if left < 5:
+            buffer = 0b1111111111111111 & ((buffer << 8) | message[idx])  # 最多只有12个有效bit位,b字符串按下标取出时会转换为int类型
+            left += 8
+            idx += 1
+        result.append(alphabet[0b11111 & (buffer >> (left - 5))])
+        left -= 5
+    if left:  # 防止left还有剩余
+        result.append(alphabet[0b11111 & (buffer << (5 - left))])
+    pad = len(result) % 8
     if pad:
-        result.extend([61]*(8-pad)) # 长度必须是8的整倍数,不足的用=填充
+        result.extend([61] * (8 - pad))  # 长度必须是8的整倍数,不足的用=填充
     return result
 
-def encode_base32_v1(s):    # 参考python3源码base64.b32encode
+
+def encode_base32_v2(message):
+    idx = 0
+    length = len(message)
+    result = bytearray()
+    left = 8
+    while idx < length:
+        if left >= 5:
+            result.append(alphabet[message[idx] >> (left - 5) & 0b00011111])
+            left -= 5
+        if idx + 1 == length:
+            break
+        else:
+            _idx = (message[idx] << (5 - left) | message[idx + 1] >> (3 + left)) & 0b00011111
+            result.append(alphabet[_idx])
+            idx += 1
+            left += 3
+    if left:  # 防止left还有剩余
+        result.append(alphabet[message[idx] << (5 - left) & 0b11111])
+    pad = len(result) % 8
+    if pad:
+        result.extend([61] * (8 - pad))  # 长度必须是8的整倍数,不足的用=填充
+    return result
+
+
+def encode_base32_v1(s):  # 参考python3源码base64.b32encode
     global _b32tab2
     # Delay the initialization of the table to not waste memory if the function is never called
     if _b32tab2 is None:
@@ -42,12 +70,13 @@ def encode_base32_v1(s):    # 参考python3源码base64.b32encode
     encoded = bytearray()
     from_bytes = int.from_bytes
     for i in range(0, len(s), 5):
-        c = from_bytes(s[i: i + 5], 'big')  # int.from_bytes(b'ab','big') == 0b0110000101100010; int.from_bytes(b'ab','little') == 0b0110001001100001
+        # int.from_bytes(b'ab','big') == 0b0110000101100010; int.from_bytes(b'ab','little') == 0b0110001001100001
+        c = from_bytes(s[i: i + 5], 'big')
         encoded += (
-            _b32tab2[c >> 30] +           # bits 1 - 10
-            _b32tab2[(c >> 20) & 0x3ff] + # bits 11 - 20
-            _b32tab2[(c >> 10) & 0x3ff] + # bits 21 - 30
-            _b32tab2[c & 0x3ff]           # bits 31 - 40
+                _b32tab2[c >> 30] +  # bits 1 - 10
+                _b32tab2[(c >> 20) & 0x3ff] +  # bits 11 - 20
+                _b32tab2[(c >> 10) & 0x3ff] +  # bits 21 - 30
+                _b32tab2[c & 0x3ff]  # bits 31 - 40
         )
     # Adjust for any leftover partial quanta
     if leftover == 1:
@@ -60,24 +89,30 @@ def encode_base32_v1(s):    # 参考python3源码base64.b32encode
         encoded[-1:] = b'='
     return bytes(encoded)
 
+
 def decode_base32(message):
     length = len(message)
     left = 0
     idx = 0
     buffer = 0
-    result=bytearray()  
-    while idx<length:
+    result = bytearray()
+    while idx < length:
         ch = message[idx]
-        if 65<=ch<=90:   # [A,Z]
-            ch=(ch&0b11111)-1
-        elif 50<=ch<=55: # [2,7]
-            ch-=24
-        else:            # b'='
+        if 65 <= ch <= 90:  # [A,Z]
+            ch = (ch & 0b11111) - 1
+        elif 50 <= ch <= 55:  # [2,7]
+            ch -= 24
+        else:  # b'='
             break
-        idx+=1
-        left+=5
-        buffer = 0b1111111111111111 & ((buffer<<5) | ch)
-        if left>=8:
-            result.append(0b11111111&(buffer>>(left-8)))
-            left-=8
+        idx += 1
+        left += 5
+        buffer = 0b1111111111111111 & ((buffer << 5) | ch)
+        if left >= 8:
+            result.append(0b11111111 & (buffer >> (left - 8)))
+            left -= 8
     return result  # 不用考虑left还有剩余,应为剩余位都是编码时补的0
+
+
+if __name__ == "__main__":
+    x = b"abcde"
+
--- base58.py
+++ base58.py
@@ -1,80 +1,86 @@
-'''
-Implementations of Base58 and Base58Check endcodings that are compatible with the bitcoin network.
+"""
+Implementations of Base58 and Base58Check encodings that are compatible with the bitcoin network.
 经过Base58编码的数据为原始的数据长度的8/log(2,58)倍,稍稍多于Base64的8/6=1.33倍
-'''
+"""
 
 from hashlib import sha256
 
 alphabet = b'123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 mapping = [
-    -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-    -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-    -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-    -1, 0, 1, 2, 3, 4, 5, 6,  7, 8,-1,-1,-1,-1,-1,-1,
-    -1, 9,10,11,12,13,14,15, 16,-1,17,18,19,20,21,-1,
-    22,23,24,25,26,27,28,29, 30,31,32,-1,-1,-1,-1,-1,
-    -1,33,34,35,36,37,38,39, 40,41,42,43,-1,44,45,46,
-    47,48,49,50,51,52,53,54, 55,56,57,-1,-1,-1,-1,-1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, -1, -1, -1, -1, -1, -1,
+    -1, 9, 10, 11, 12, 13, 14, 15, 16, -1, 17, 18, 19, 20, 21, -1,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, -1, -1, -1, -1, -1,
+    -1, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, -1, 44, 45, 46,
+    47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, -1, -1, -1, -1, -1,
 ]
-sha=lambda message:sha256(sha256(message).digest()).digest()
+sha = lambda message: sha256(sha256(message).digest()).digest()
+
 
 def encode_base58(v):
     if isinstance(v, str):
         v = v.encode('utf8')
-    origlen = len(v)    
-    v = v.lstrip(b'\0') # Skip & count leading zeroes.
-    zeroes=origlen-len(v)
-    string = bytearray(b'1'*zeroes)
-    acc=0
+    origen_len = len(v)
+    v = v.lstrip(b'\0')  # Skip & count leading zeroes.
+    zeroes = origen_len - len(v)
+    string = bytearray(b'1' * zeroes)
+    acc = 0
     for each in v:
-        acc=(acc<<8)|each
+        acc = (acc << 8) | each
     # acc = int(binascii.hexlify(v),16) # big-endian,if v=b'ab' then acc=0b0110000101100010
     while acc:
         acc, idx = divmod(acc, 58)
-        string.insert(zeroes,alphabet[idx])
+        string.insert(zeroes, alphabet[idx])
     return string
 
+
 def decode_base58(v):
-    ones=0
-    while v[ones]==49: # '1'的ascii码
-        ones+=1
+    ones = 0
+    while v[ones] == 49:  # '1'的ascii码
+        ones += 1
     acc = 0
     for char in v[ones:]:
         acc = acc * 58 + mapping[char]
     result = bytearray()
     while acc:
         acc, mod = divmod(acc, 256)
-        result.insert(0,mod)
+        result.insert(0, mod)
     return b'\0' * ones + result
+
 
 def _encode_base58(v):
     if isinstance(v, str):
         v = v.encode('utf8')
-    origlen = len(v)    
-    v = v.lstrip(b'\0') # Skip & count leading zeroes.
-    newlen=len(v) 
-    size=(newlen*138//100+1) # log(256) / log(58), rounded up.Allocate enough space in big-endian base58 representation.
-    buffer=[0]*size
-    length=0
+    origen_len = len(v)
+    v = v.lstrip(b'\0')  # Skip & count leading zeroes.
+    new_len = len(v)
+    # log(256) / log(58), rounded up.Allocate enough space in big-endian base58 representation.
+    size = (new_len * 138 // 100 + 1)
+    buffer = [0] * size
+    length = 0
     for carry in v:
-        right=size-1
-        _length=0
-        while carry or _length<length:
-            carry+=buffer[right]<<8
-            buffer[right]=carry%58
-            carry//=58
-            right-=1
-            _length+=1
-        length=_length
-    string = bytearray(b'1'*(origlen-newlen))
-    for idx in buffer[right+1:]:
+        right = size - 1
+        _length = 0
+        while carry or _length < length:
+            carry += buffer[right] << 8
+            buffer[right] = carry % 58
+            carry //= 58
+            right -= 1
+            _length += 1
+        length = _length
+    string = bytearray(b'1' * (origen_len - new_len))
+    for idx in buffer[right + 1:]:
         string.append(alphabet[idx])
     return string
+
 
 def encode_check(v):
     if isinstance(v, str):
         v = v.encode('utf8')
     return encode_base58(v + sha(v)[:4])
+
 
 def decode_check(v):
     v = decode_base58(v)
@@ -83,10 +89,11 @@ def decode_check(v):
         raise ValueError("Invalid checksum")
     return result
 
+
 if __name__ == '__main__':
-    print(encode_base58('hello world'))                    # bytearray(b'StV1DL6CwTryKyV')
-    print(_encode_base58('hello world'))                   # bytearray(b'StV1DL6CwTryKyV')
-    print(decode_base58(bytearray(b'StV1DL6CwTryKyV')))    # b'hello world'
-    print(encode_check('akatsuki'))                        # bytearray(b'2qdLm9BNJAkjpnXz3')
-    print(decode_check(bytearray(b'2qdLm9BNJAkjpnXz3')))   # b'akatsuki'
+    print(encode_base58('hello world'))  # bytearray(b'StV1DL6CwTryKyV')
+    print(_encode_base58('hello world'))  # bytearray(b'StV1DL6CwTryKyV')
+    print(decode_base58(bytearray(b'StV1DL6CwTryKyV')))  # b'hello world'
+    print(encode_check('akatsuki'))  # bytearray(b'2qdLm9BNJAkjpnXz3')
+    print(decode_check(bytearray(b'2qdLm9BNJAkjpnXz3')))  # b'akatsuki'
     # print(decode_check(bytearray(b'3qdLm9BNJAkjpnXz3'))) # ValueError: Invalid checksum
