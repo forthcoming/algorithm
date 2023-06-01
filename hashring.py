df372d728f23dafaba53346ba76094547424d360
neos <akatsuki10086@gmail.com>
2023-06-01 20:57:19 +0800

    Update hashring.py

--- hashring.py
+++ hashring.py
@@ -6,29 +6,20 @@ from struct import unpack
 # Implements consistent hashing that can be used when the number of server nodes can increase or decrease.
 class HashRing:
     def __init__(self, v_node_num=1):
-        self.mapping = {}
-        self.rings = []  # sorted list
-        self.storage = {}
+        self.rings = []  # sorted list,只存储节点哈希值
+        self.storage = {}  # 节点哈希值与存储元素的映射关系
         self.v_node_num = v_node_num  # every node expand v_node_num times
         self.length = 0
 
     @staticmethod
     def _hash(value):
         k = md5(bytes(value, 'utf8')).digest()
-        return unpack("<I", k[:4])[0]
-
-    def add_items(self, items):
-        if self.length:
-            for item in items:
-                hash_value = HashRing._hash(item)
-                pos = bisect_left(self.rings, hash_value) % self.length  # 注意这里跟bisect_right的区别
-                self.storage[self.rings[pos]].append(item)
+        return unpack("<I", k[:4])[0]  # unsigned int
 
     def add_node(self, node):
-        for index in range(0, self.v_node_num):
+        for index in range(self.v_node_num):
             hash_value = HashRing._hash('{}#{}'.format(node, index))
-            if hash_value not in self.mapping:  # 小概率事件,但也要避免,保证环里面的hash_value唯一
-                self.mapping[hash_value] = node
+            if hash_value not in self.storage:  # 小概率事件,但也要避免,保证环里面的hash_value唯一
                 self.storage[hash_value] = []
                 if self.length:
                     pos = bisect_left(self.rings, hash_value) % self.length
@@ -41,6 +32,13 @@ class HashRing:
                     self.rings.append(hash_value)
                 self.length += 1
 
+    def add_items(self, items):
+        if self.length:
+            for item in items:
+                hash_value = HashRing._hash(item)
+                pos = bisect_left(self.rings, hash_value) % self.length  # 注意这里跟bisect_right的区别
+                self.storage[self.rings[pos]].append(item)
+
     def remove_node(self, node):
         pass
 
@@ -50,7 +48,6 @@ if __name__ == '__main__':
     for node in range(4):
         ring.add_node(node)
     ring.add_items(map(str, range(100)))
-    print(ring.mapping)
     print(ring.rings)
     print(ring.length)
     print(ring.storage)
