13cf31500ad5a78cfc1ee7fc280f5a92c930fb32
neos <akatsuki10086@gmail.com>
2023-06-02 02:32:43 +0800

    update

--- sort.py
+++ sort.py
@@ -1,228 +1,235 @@
 # 基数排序(时间复杂度为O(d*n),特别适合待排记录数n很大而关键字位数d很小的自然数)
-def RadixSort(li,radix=10):
-    num=1<<radix
-    length=len(li)
-    bucket=[None]*length
-    count=[0]*num
-    getBit=lambda x,y:x>>(y*radix)&(num-1)
-    bit=0
+def radix_sort(arr, radix=10):
+    num = 1 << radix
+    length = len(arr)
+    bucket = [None] * length
+    count = [0] * num
+    get_bit = lambda x, y: x >> (y * radix) & (num - 1)
+    bit = 0
     while True:
-        for i in li:
-            count[getBit(i,bit)]+=1  #有点计数排序的思想
-        if count[0]==length:   #说明已经遍历完所有位
+        for i in arr:
+            count[get_bit(i, bit)] += 1  # 有点计数排序的思想
+        if count[0] == length:  # 说明已经遍历完所有位
             break
-        #for j in range(len(count)-2,-1,-1):  #逆序
-            #count[j]+=count[j+1]
-        for j in range(1,len(count)):
-            count[j]+=count[j-1]
-        for k in li[::-1]:  #注意这里要逆序遍历
-            index=getBit(k,bit)
-            bucket[count[index]-1]=k
-            count[index]-=1
-        li[:]=bucket   #li=bucket并不会改变外面的li对象
-        count=[0]*num
-        bit+=1
-        
+        # for j in range(len(count) - 2, -1, -1):  # 逆序
+        #     count[j] += count[j + 1]
+        for j in range(1, len(count)):
+            count[j] += count[j - 1]
+        for k in arr[::-1]:  # 注意这里要逆序遍历
+            index = get_bit(k, bit)
+            bucket[count[index] - 1] = k
+            count[index] -= 1
+        arr[:] = bucket  # li=bucket并不会改变外面的li对象
+        count = [0] * num
+        bit += 1
+
+
 # MSD-10进制递归版基数排序
-def MSDRadixSort(li,left,right,N=5,radix=10):
-    if N and left<right:
-        bucket=[None]*(right-left+1)
-        count=[0]*radix
-        getBit=lambda x,y:x//radix**(y-1)%radix
-        for i in li[left:right+1]:
-            count[getBit(i,N)]+=1
-        for j in range(1,radix):
-            count[j]+=count[j-1]
-        for k in li[left:right+1]:  # 正序逆序都可以
-            index=getBit(k,N)
-            bucket[count[index]-1]=k
-            count[index]-=1
-        li[left:right+1]=bucket  #注意这里要加1
-        N-=1
-        for x in range(0,radix-1):  #遍历count每一个元素
-            MSDRadixSort(li,left+count[x],left+count[x+1]-1,N,radix)  # attention
-        MSDRadixSort(li,left+count[-1],right,N,radix)   # attention
-        
+def msd_radix_sort(arr, left, right, n=5, radix=10):
+    if n and left < right:
+        bucket = [None] * (right - left + 1)
+        count = [0] * radix
+        get_bit = lambda x, y: x // radix ** (y - 1) % radix
+        for i in arr[left:right + 1]:
+            count[get_bit(i, n)] += 1
+        for j in range(1, radix):
+            count[j] += count[j - 1]
+        for k in arr[left:right + 1]:  # 正序逆序都可以
+            index = get_bit(k, n)
+            bucket[count[index] - 1] = k
+            count[index] -= 1
+        arr[left:right + 1] = bucket  # 注意这里要加1
+        n -= 1
+        for x in range(0, radix - 1):  # 遍历count每一个元素
+            msd_radix_sort(arr, left + count[x], left + count[x + 1] - 1, n, radix)  # attention
+        msd_radix_sort(arr, left + count[-1], right, n, radix)  # attention
+
+
 # 桶排序(效率跟基数排序类似,实质是哈希,radix越大,空间复杂度越大,时间复杂度越小,但大到一定限度后时间复杂度会增加,适用于自然数)
-def BucketSort(li,radix=10):
-    lower=(1<<radix)-1
-    bucket = [[] for i in range(lower+1)] # 不能用 [[]]*(lower+1)
-    bit=0
+def bucket_sort(arr, radix=10):
+    lower = (1 << radix) - 1
+    bucket = [[] for i in range(lower + 1)]  # 不能用 [[]]*(lower+1)
+    bit = 0
     while True:
-        for val in li:
-            bucket[val>>(bit*radix)&lower].append(val) # 很关键,原理是将自然数看成二进制数,然后再按lower个位数划分
-        if len(bucket[0])==len(li):
+        for val in arr:
+            bucket[val >> (bit * radix) & lower].append(val)  # 很关键,原理是将自然数看成二进制数,然后再按lower个位数划分
+        if len(bucket[0]) == len(arr):
             break
-        del li[:]
+        del arr[:]
         for each in bucket:
-        # for each in bucket[::-1]:   # 逆序
-            li+=each  # 部分each为[]
-            each[:]=[] # 清空桶数据
-        bit+=1
+            # for each in bucket[::-1]:   # 逆序
+            arr += each  # 部分each为[]
+            each[:] = []  # 清空桶数据
+        bit += 1
         '''
         下面2种li赋值方法效率很低
         li[:]=reduce(lambda x,y:x+y,bucket)
         li[:]=sum(bucket,[])
         '''
-        
+
+
 # 快速排序(非稳定排序)
-from random import randrange
-def QuickSort(li,left,right):  # 包含left,right边界
-    if left<right:   # 效率最高
-        flag=li[left]
-        start=left
-        end=right
-        while start<end:  #不能有等号
-            while start<end and li[end]>=flag:  #不能有等号
-                end-=1
-            li[start]=li[end]
-            while start<end and li[start]<=flag:   #不能有等号
-                start+=1
-            li[end]=li[start] 
-        li[start]=flag      #此时start等与end
-        QuickSort(li,left,start-1)
-        QuickSort(li,start+1,right)
-        
-    # if left<right:  # 效率一般
-    #     index=randrange(left,right+1)  # 防止数组本身基本有序带来的效率损失
-    #     li[left],li[index]=li[index],li[left]
-    #     mid=left
-    #     for i in range(left+1,right+1):
-    #         if li[i]<li[left]:
-    #             mid+=1
-    #             li[i],li[mid]=li[mid],li[i]
-    #     li[left],li[mid]=li[mid],li[left]
-    #     QuickSort(li,left,mid-1)
-    #     QuickSort(li,mid+1,right)
-        
-    # if left<right:  # 效率较低
-    #     key=li[left]
-    #     start,end=left+1,right
-    #     while start<=end:
-    #         if li[start]>key:
-    #             li[start],li[end]=li[end],li[start]
-    #             end-=1
+def quick_sort(arr, left, right):  # 包含left,right边界
+    if left < right:  # 效率最高
+        flag = arr[left]
+        start = left
+        end = right
+        while start < end:  # 不能有等号
+            while start < end and arr[end] >= flag:  # 不能有等号
+                end -= 1
+            arr[start] = arr[end]
+            while start < end and arr[start] <= flag:  # 不能有等号
+                start += 1
+            arr[end] = arr[start]
+        arr[start] = flag  # 此时start等与end
+        quick_sort(arr, left, start - 1)
+        quick_sort(arr, start + 1, right)
+
+    # if left < right:  # 效率一般
+    #     index = randrange(left, right + 1)  # 防止数组本身基本有序带来的效率损失
+    #     arr[left], arr[index] = arr[index], arr[left]
+    #     mid = left
+    #     for i in range(left + 1, right + 1):
+    #         if arr[i] < arr[left]:
+    #             mid += 1
+    #             arr[i], arr[mid] = arr[mid], arr[i]
+    #     arr[left], arr[mid] = arr[mid], arr[left]
+    #     quick_sort(arr, left, mid - 1)
+    #     quick_sort(arr, mid + 1, right)
+    #
+    # if left < right:  # 效率较低
+    #     key = arr[left]
+    #     start, end = left + 1, right
+    #     while start <= end:
+    #         if arr[start] > key:
+    #             arr[start], arr[end] = arr[end], arr[start]
+    #             end -= 1
     #         else:
-    #             start+=1     
-    #     li[start-1],li[left]=li[left],li[start-1]
-    #     QuickSort(li,left,start-2)
-    #     QuickSort(li,start,right)
-    
+    #             start += 1
+    #     arr[start - 1], arr[left] = arr[left], arr[start - 1]
+    #     quick_sort(arr, left, start - 2)
+    #     quick_sort(arr, start, right)
+
+
 # 希尔排序
-def ShellSort(li):
-    length=len(li)
-    step=length>>1
+def shell_sort(arr):
+    length = len(arr)
+    step = length >> 1
     # while step:
-    #     for i in range(step):  #遍历每个分组
-    #         for j in range(i+step,length,step):
-    #             tmp=li[j]
-    #             while j>=step and tmp<li[j-step]:
-    #                 li[j]=li[j-step]
-    #                 j-=step
-    #             li[j]=tmp
-    #     step>>=1
+    #     for i in range(step):  # 遍历每个分组
+    #         for j in range(i + step, length, step):
+    #             tmp = arr[j]
+    #             while j >= step and tmp < arr[j - step]:
+    #                 arr[j] = arr[j - step]
+    #                 j -= step
+    #             arr[j] = tmp
+    #     step >>= 1
 
-    while step:  #效率与上面一样,只不过从不同的方向思考问题
-        for i in range(step,length):  #遍历每一个数组元素,然后再跟其对应的分组元素做比较
-            index=i-step
-            tmp=li[i]
-            while index>=0 and li[index]>tmp:
-                li[index+step]=li[index]
-                index-=step
-            li[index+step]=tmp
-        step>>=1
-        
+    while step:  # 效率与上面一样,只不过从不同的方向思考问题
+        for i in range(step, length):  # 遍历每一个数组元素,然后再跟其对应的分组元素做比较
+            index = i - step
+            tmp = arr[i]
+            while index >= 0 and arr[index] > tmp:
+                arr[index + step] = arr[index]
+                index -= step
+            arr[index + step] = tmp
+        step >>= 1
+
+
 # 位排序(仅适用于不重复的自然数)
-def BitSort(li):   
+def bit_sort(arr):
     import math
-    bitmap=0
-    for each in li:
-        bitmap|=1<<each
+    bitmap = 0
+    for each in arr:
+        bitmap |= 1 << each
 
-    while bitmap:  #适用于数的范围较零散
-        t=math.log2(bitmap&-bitmap) #x&-x返回最低位1
-        print(int(t),end=' ')
-        bitmap&=bitmap-1            #x&=x-1清除最低位1
+    while bitmap:  # 适用于数的范围较零散
+        t = math.log2(bitmap & -bitmap)  # x&-x返回最低位1
+        print(int(t), end=' ')
+        bitmap &= bitmap - 1  # x&=x-1清除最低位1
 
-    # index=-1       #适用于数的范围较稠密
+    # index = -1  # 适用于数的范围较稠密
     # while bitmap:
-    #     index+=1
-    #     if bitmap&1:
-    #         print(index,end=' ')
-    #     bitmap>>=1   
-    
+    #     index += 1
+    #     if bitmap & 1:
+    #         print(index, end=' ')
+    #     bitmap >>= 1
+
+
 # 冒泡排序
-def BubbleSort(li):  #针对此类[random.randrange(0,1000,3) for i in range(2000)]+list(range(3000))大数基本靠右的效率更高
-    lastChange=len(li)-1
-    flag=-1
-    while flag!=lastChange:
-        flag=lastChange
-        for i in range(lastChange):
-            if li[i]>li[i+1]:
-                li[i],li[i+1]=li[i+1],li[i]
-                lastChange=i
-   
-    # length=len(li)
-    # flag=True
-    # for i in range(1,length):   #控制次数
-    #     for j in range(length-i):
-    #         if li[j]>li[j+1]:
-    #             li[j],li[j+1]=li[j+1],li[j]
-    #             flag=False
+def bubble_sort(arr):  # 针对此类[random.randrange(0,1000,3) for i in range(2000)]+list(range(3000))大数基本靠右的效率更高
+    last_change = len(arr) - 1
+    flag = -1
+    while flag != last_change:
+        flag = last_change
+        for i in range(last_change):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                last_change = i
+
+    # length = len(arr)
+    # flag = True
+    # for i in range(1, length):  # 控制次数
+    #     for j in range(length - i):
+    #         if arr[j] > arr[j + 1]:
+    #             arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    #             flag = False
     #     if flag:
     #         break
 
+
 # 地精排序
-def GnomeSort(li):
-    length=len(li)
-    i=1
-    while i!=length:
-        if i and li[i]<li[i-1]:
-            li[i],li[i-1]=li[i-1],li[i]
-            i-=1
+def gnome_sort(arr):
+    length = len(arr)
+    i = 1
+    while i != length:
+        if i and arr[i] < arr[i - 1]:
+            arr[i], arr[i - 1] = arr[i - 1], arr[i]
+            i -= 1
         else:
-            i+=1
+            i += 1
+
 
 # 插入排序
-def InsertSort(li):
-    length=len(li)
-    for i in range(1,length):
-        tmp=li[i]
-        while i and tmp<li[i-1]:
-            li[i]=li[i-1]  # 避免相邻两项两两交换
-            i-=1              
-        li[i]=tmp
-        
-    # for i in range(1,length): #效率较低
-    #     for j in range(i,0,-1):
-    #         if li[j]<li[j-1]:
-    #             li[j],li[j-1]=li[j-1],li[j]
+def insert_sort(arr):
+    length = len(arr)
+    for i in range(1, length):
+        tmp = arr[i]
+        while i and tmp < arr[i - 1]:
+            arr[i] = arr[i - 1]  # 避免相邻两项两两交换
+            i -= 1
+        arr[i] = tmp
+
+    # for i in range(1, length):  # 效率较低
+    #     for j in range(i, 0, -1):
+    #         if arr[j] < arr[j - 1]:
+    #             arr[j], arr[j - 1] = arr[j - 1], arr[j]
     #         else:
     #             break
 
-# 选择排序(非稳定排序)
-def SelectSort(li):
-    length=len(li)
-    for i in range(1,length):
-        index=i-1
-        for j in range(i,length):
-            if li[index]>li[j]:
-                index=j
-        li[index],li[i-1]=li[i-1],li[index]
 
-    # left=0
-    # right=len(li)-1
-    # while left<right:
-    #     big=small=left
-    #     for i in range(left+1,right+1):
-    #         if li[i]>li[big]:
-    #             big=i
-    #         elif li[i]<li[small]:
-    #             small=i
-    #     li[left],li[small]=li[small],li[left]
-    #     if big==left:  # 注意判断
-    #         big=small
-    #     li[right],li[big]=li[big],li[right]
-    #     left+=1
-    #     right-=1
-    
+# 选择排序(非稳定排序)
+def select_sort(arr):
+    length = len(arr)
+    for i in range(1, length):
+        index = i - 1
+        for j in range(i, length):
+            if arr[index] > arr[j]:
+                index = j
+        arr[index], arr[i - 1] = arr[i - 1], arr[index]
+
+    # left = 0
+    # right = len(arr) - 1
+    # while left < right:
+    #     big = small = left
+    #     for i in range(left + 1, right + 1):
+    #         if arr[i] > arr[big]:
+    #             big = i
+    #         elif arr[i] < arr[small]:
+    #             small = i
+    #     arr[left], arr[small] = arr[small], arr[left]
+    #     if big == left:  # 注意判断
+    #         big = small
+    #     arr[right], arr[big] = arr[big], arr[right]
+    #     left += 1
+    #     right -= 1
--- unionfind.py
+++ unionfind.py
@@ -1,42 +1,42 @@
 # 并查集
 class UnionFindSet:
-    def __init__(self,sets=[-4,0,1,1,-6,4,4,5,7,5]): #非负数指向父节点的下标,负数n代表该根节点下有|n|个孩子
-        '''
+    def __init__(self, sets=[-4, 0, 1, 1, -6, 4, 4, 5, 7, 5]):  # 非负数指向父节点的下标,负数n代表该根节点下有|n|个孩子
+        """
              0       4
-             |      / \            
-             1     5   6    
-            / \   / \              
+             |      / \
+             1     5   6
+            / \   / \
            2   3  7  9
                   |
                   8
-        '''
-        self.__set=sets
+        """
+        self.__set = sets
 
-    def union(self,i,j):
-        I=self.find(i)
-        J=self.find(j)
-        if I!=J:
-            if self.__set[I]>self.__set[J]:  #__set[i]集合元素个数更少(将小结果集追加到大结果集中)
-                self.__set[J]+=self.__set[I]
-                self.__set[I]=J
+    def union(self, i, j):
+        I = self.find(i)
+        J = self.find(j)
+        if I != J:
+            if self.__set[I] > self.__set[J]:  # __set[i]集合元素个数更少(将小结果集追加到大结果集中)
+                self.__set[J] += self.__set[I]
+                self.__set[I] = J
             else:
-                self.__set[I]+=self.__set[J]
-                self.__set[J]=I
+                self.__set[I] += self.__set[J]
+                self.__set[J] = I
             return True
         return False
 
-    def find(self,i):  #路径压缩算法(查找父亲的同时减小树的深度,路径上的子节点在查找完一遍后通通指向父节点)
-        if self.__set[i]>=0:
-            self.__set[i]=self.find(self.__set[i])
+    def rec_find(self, i):  # 路径压缩算法(查找父亲的同时减小树的深度,路径上的子节点在查找完一遍后通通指向父节点)
+        if self.__set[i] >= 0:
+            self.__set[i] = self.rec_find(self.__set[i])
             return self.__set[i]
         return i
-    
-    def find(self,i):   # 迭代版路径压缩算法
+
+    def find(self, i):  # 迭代版路径压缩算法
         root = i
-        while self.__set[root]>=0:
-            root=self.__set[root]
-        while root!=i:
-            k=self.__set[i]
-            self.__set[i]=root
-            i=k
+        while self.__set[root] >= 0:
+            root = self.__set[root]
+        while root != i:
+            k = self.__set[i]
+            self.__set[i] = root
+            i = k
         return root
