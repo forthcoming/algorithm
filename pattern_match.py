c0de5fbf1e85f7c43dc1c1848af37fc31f8167bd
neos <akatsuki10086@gmail.com>
2023-06-04 17:16:16 +0800

    Update pattern_match.py

--- pattern_match.py
+++ pattern_match.py
@@ -1,74 +1,77 @@
-def BF(S,T,pos=0):
-    sLength=len(S)
-    tLength=len(T)
-    j=0
-    while pos+j<sLength and j<tLength:
-        if S[pos+j]==T[j]:
-            j+=1
+def bf(source, pattern, pos=0):
+    source_length = len(source)
+    pattern_length = len(pattern)
+    j = 0
+    while pos + j < source_length and j < pattern_length:
+        if source[pos + j] == pattern[j]:
+            j += 1
         else:
-            pos+=1
-            j=0
-    return pos if j==tLength else -1
+            pos += 1
+            j = 0
+    return pos if j == pattern_length else -1
 
-def KMP(S,T,pos=0):
-    sLength=len(S)
-    tLength=len(T)
+
+def kmp(source, pattern, pos=0):
+    source_length = len(source)
+    pattern_length = len(pattern)
     # 求串T的next数组
-    _next=[-1]*tLength
-    k=-1
-    index=1
-    while index<tLength:
-        if k==-1 or T[index-1]==T[k]:
-            k+=1
+    _next = [-1] * pattern_length
+    k = -1
+    index = 1
+    while index < pattern_length:
+        if k == -1 or pattern[index - 1] == pattern[k]:
+            k += 1
             # 优化_next数组,直接_next[index]=k也行,数组_next中的数字越小则认为越优化
-            _next[index]=_next[k] if T[index]==T[k] else k
-            index+=1
+            _next[index] = _next[k] if pattern[index] == pattern[k] else k
+            index += 1
         else:
-            k=_next[k]
+            k = _next[k]
 
     print(_next)
-    index=0
-    while index<tLength and pos<sLength:
-        if index==-1 or S[pos]==T[index]:
-            pos+=1
-            index+=1
+    index = 0
+    while index < pattern_length and pos < source_length:
+        if index == -1 or source[pos] == pattern[index]:
+            pos += 1
+            index += 1
         else:
-            index=_next[index]
-    return pos-index if index==tLength else -1
+            index = _next[index]
+    return pos - index if index == pattern_length else -1
 
-def Sunday(S,T,pos=0):
-    sLength=len(S)
-    tLength=len(T)
-    hashTable={each:index for index,each in enumerate(T)}  # 重要,后面出现的字符index覆盖前面重复出现的字符index
-    index=0
-    while index<tLength and pos<sLength:
-        if T[index]==S[pos]:
-            pos+=1
-            index+=1
+
+def sunday(source, pattern, pos=0):
+    source_length = len(source)
+    pattern_length = len(pattern)
+    hash_table = {each: index for index, each in enumerate(pattern)}  # 重要,后面出现的字符index覆盖前面重复出现的字符index
+    index = 0
+    while index < pattern_length and pos < source_length:
+        if pattern[index] == source[pos]:
+            pos += 1
+            index += 1
         else:
-            pos+=tLength-index      # 注意这里的位置
-            if pos>=sLength:
+            pos += pattern_length - index  # 注意这里的位置
+            if pos >= source_length:
                 return -1
             else:
-                pos-=hashTable.get(S[pos],-1)   # 注意这里如果没找到则返回-1
-                index=0
-    return pos-index if index==tLength else -1
+                pos -= hash_table.get(source[pos], -1)  # 注意这里如果没找到则返回-1
+                index = 0
+    return pos - index if index == pattern_length else -1
 
-def rollingHash(source,pattern,radix=31):
-    sourceLength=len(source)
-    patternLength=len(pattern)
-    if sourceLength<patternLength:
+
+def rolling_hash(source, pattern, radix=31):
+    source_length = len(source)
+    pattern_length = len(pattern)
+    if source_length < pattern_length:
         return -1
-    sourceHash=0
-    patternHash=0
-    offset=radix**patternLength
-    for i in range(patternLength):
-        sourceHash=sourceHash*radix+ord(source[i])
-        patternHash=patternHash*radix+ord(pattern[i])
-    if sourceHash==patternHash:
+    source_hash = 0
+    pattern_hash = 0
+    offset = radix ** pattern_length
+    for i in range(pattern_length):
+        source_hash = source_hash * radix + ord(source[i])
+        pattern_hash = pattern_hash * radix + ord(pattern[i])
+    if source_hash == pattern_hash:
         return 0
-    for j in range(patternLength,sourceLength):
-        sourceHash=sourceHash*radix+ord(source[j])-ord(source[j-patternLength])*offset
-        if sourceHash==patternHash:  # 此处可以做进一步检查,字符串是否相等
-            return j-patternLength+1
+    for j in range(pattern_length, source_length):
+        source_hash = source_hash * radix + ord(source[j]) - ord(source[j - pattern_length]) * offset
+        if source_hash == pattern_hash:  # 此处可以做进一步检查,字符串是否相等
+            return j - pattern_length + 1
     return -1
