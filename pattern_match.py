828d4d5be721d0d3455af99a3c94857ddb8c7a06
neos <akatsuki10086@gmail.com>
2023-06-05 18:12:29 +0800

    Update pattern_match.py

--- pattern_match.py
+++ pattern_match.py
@@ -1,33 +1,33 @@
 def bf(source, pattern, pos=0):
-    source_length = len(source)
-    pattern_length = len(pattern)
+    length_s = len(source)
+    length_p = len(pattern)
     j = 0
-    while pos + j < source_length and j < pattern_length:
+    while pos + j < length_s and j < length_p:
         if source[pos + j] == pattern[j]:
             j += 1
         else:
             pos += 1
             j = 0
-    return pos if j == pattern_length else -1
+    return pos if j == length_p else -1
 
 
-def rolling_hash(source, pattern, radix=31):
-    source_length = len(source)
-    pattern_length = len(pattern)
-    if source_length < pattern_length:
+def rabin_karp(source, pattern, radix=31):
+    length_s = len(source)
+    length_p = len(pattern)
+    if length_s < length_p:
         return -1
     source_hash = 0
     pattern_hash = 0
-    offset = radix ** pattern_length
-    for i in range(pattern_length):
-        source_hash = source_hash * radix + ord(source[i])
+    offset = radix ** length_p
+    for i in range(length_p):
+        source_hash = source_hash * radix + ord(source[i])  # rolling hash
         pattern_hash = pattern_hash * radix + ord(pattern[i])
     if source_hash == pattern_hash:  # 此处可以做进一步检查,字符串是否相等
         return 0
-    for j in range(pattern_length, source_length):
-        source_hash = source_hash * radix - ord(source[j - pattern_length]) * offset + ord(source[j])
+    for j in range(length_p, length_s):
+        source_hash = source_hash * radix - ord(source[j - length_p]) * offset + ord(source[j])
         if source_hash == pattern_hash:  # 此处可以做进一步检查,字符串是否相等
-            return j - pattern_length + 1
+            return j - length_p + 1
     return -1
 
 
@@ -44,56 +44,56 @@ def build_next(pattern):
       1. 如果p[next[j]]==p[j],next[j+1]=next[j]+1,如果next[j+1]再大点next[j]一定会变大,所以next[j+1]只能等于next[j]+1
       2. 如果p[next[j]]!=p[j],需要在p[0,next[j]-1]子串寻找,即判断p[next[next[j]]]是否等于p[j]
     """
-    pattern_length = len(pattern)
-    _next = [-1] * pattern_length
-    k = -1  # 代表next[p_pos-1]
-    p_pos = 1
-    while p_pos < pattern_length:
-        if k == -1 or pattern[p_pos - 1] == pattern[k]:
+    length_p = len(pattern)
+    _next = [-1] * length_p
+    k = -1  # 代表优化前的next[pos_p-1]
+    pos_p = 1
+    while pos_p < length_p:
+        if k == -1 or pattern[pos_p - 1] == pattern[k]:
             k += 1
-            # 优化_next数组,直接_next[p_pos]=k也行,数组_next中的数字越小则认为越优化
-            if pattern[p_pos] == pattern[k]:
-                _next[p_pos] = _next[k]
-                # _next[p_pos] = k
-            else:
-                _next[p_pos] = k
-            p_pos += 1
+            # 优化_next数组,直接_next[pos_p]=k也行,数组_next中的数字越小则认为越优化
+            k1 = k
+            while k1 != -1 and pattern[pos_p] == pattern[k1]:  # pattern[pos_p]代表匹配失败的字符
+                k1 = _next[k1]
+            _next[pos_p] = k1
+            pos_p += 1
         else:
             k = _next[k]
     return _next
 
 
-def kmp(source, pattern, s_pos=0):  # 时间复杂度O(s+p)
-    source_length = len(source)
-    pattern_length = len(pattern)
+def kmp(source, pattern, pos_s=0):  # 时间复杂度O(s+p)
+    length_s = len(source)
+    length_p = len(pattern)
     _next = build_next(pattern)
-    p_pos = 0
-    while p_pos < pattern_length and s_pos < source_length:
-        if p_pos == -1 or source[s_pos] == pattern[p_pos]:
-            s_pos += 1
-            p_pos += 1
+    pos_p = 0
+    while pos_p < length_p and pos_s < length_s:
+        if pos_p == -1 or source[pos_s] == pattern[pos_p]:
+            pos_s += 1
+            pos_p += 1
         else:
-            p_pos = _next[p_pos]
-    return s_pos - p_pos if p_pos == pattern_length else -1
+            pos_p = _next[pos_p]
+    return pos_s - pos_p if pos_p == length_p else -1
 
 
-def sunday(source, pattern, pos=0):
-    source_length = len(source)
-    pattern_length = len(pattern)
-    hash_table = {each: index for index, each in enumerate(pattern)}  # 重要,后面出现的字符index覆盖前面重复出现的字符index
-    index = 0
-    while index < pattern_length and pos < source_length:
-        if pattern[index] == source[pos]:
-            pos += 1
-            index += 1
+def sunday(source, pattern, pos_s=0):
+    length_s = len(source)
+    length_p = len(pattern)
+    mapping = {each: index for index, each in enumerate(pattern)}  # 重要,后面出现的字符index覆盖前面重复出现的字符index
+    pos_p = 0
+    while pos_p < length_p and pos_s < length_s:
+        if pattern[pos_p] == source[pos_s]:
+            pos_s += 1
+            pos_p += 1
         else:
-            pos += pattern_length - index  # 注意这里的位置
-            if pos >= source_length:
+            pos_s += length_p - pos_p  # 匹配失败时pos_s是source中参加匹配的最末位字符的下一位字符下标,应为这个字符一定会被比较
+            if pos_s >= length_s:
                 return -1
             else:
-                pos -= hash_table.get(source[pos], -1)  # 注意这里如果没找到则返回-1
-                index = 0
-    return pos - index if index == pattern_length else -1
+                # 将source和pattern的source[pos_s]字符对齐,然后从头开始遍历,没找到返回-1,从pos_s下一个位置开始遍历
+                pos_s -= mapping.get(source[pos_s], -1)
+                pos_p = 0
+    return pos_s - pos_p if pos_p == length_p else -1
 
 
 if __name__ == "__main__":
