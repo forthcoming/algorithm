313cca68d8a3d3d6a2ed654c26c9841edbbca57a
neos <akatsuki10086@gmail.com>
2023-06-07 14:26:22 +0800

    Update loser_tree.py

--- loser_tree.py
+++ loser_tree.py
@@ -1,9 +1,10 @@
 class LoserTree:
     def __init__(self, arr):
         length = len(arr)
+        self.max_value = float("inf")
         self.node = [length] * length
         self.count = length
-        self.leaves = arr + [-float("inf")]
+        self.leaves = arr + [-self.max_value]
         for index in range(length - 1, -1, -1):
             self.adjust(index)
 
@@ -11,25 +12,21 @@ class LoserTree:
         parent = (self.count + index) >> 1
         while parent > 0:
             if self.leaves[index] > self.leaves[self.node[parent]]:
-                # 值大者败
-                # 进入此条件，说明leaves[index]是败者。所以对它的父节点进行赋值
+                # 说明leaves[index]是败者,所以对它的父节点进行赋值,交换后index变成了优胜者
                 self.node[parent], index = index, self.node[parent]
-                # 交换后index变成了优胜者
-                # 求出parent的parent,进入下一轮循环
             parent >>= 1
         self.node[0] = index  # 循环结束后index一定是最后的优胜者
 
     def sort(self):
         if self.count <= 0:
             return
-        max_value = float("inf")
         while True:
             min_pos = self.node[0]
             min_value = self.leaves[min_pos]
-            if min_value == max_value:
+            if min_value == self.max_value:
                 break
             print(min_value)
-            self.leaves[min_pos] = max_value
+            self.leaves[min_pos] = self.max_value
             self.adjust(min_pos)
 
 
