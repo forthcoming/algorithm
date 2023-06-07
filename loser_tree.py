67b5d2eb080e60ee9bc039f7f6a539a1d47f6de9
neos <akatsuki10086@gmail.com>
2023-06-07 14:40:44 +0800

    Update loser_tree.py

--- loser_tree.py
+++ loser_tree.py
@@ -1,17 +1,18 @@
 class LoserTree:
-    def __init__(self, arr):
+    def __init__(self, arr, key=lambda x, y: x > y):
         length = len(arr)
-        self.max_value = float("inf")
-        self.node = [length] * length
+        self.key = key
+        self.extreme_value = float("inf") if key(1, 0) else -float("inf")
+        self.node = [length] * length  # 初始状态败者节点都指向leaves[-1]
         self.count = length
-        self.leaves = arr + [-self.max_value]
+        self.leaves = arr + [-self.extreme_value]
         for index in range(length - 1, -1, -1):
             self.adjust(index)
 
     def adjust(self, index):
         parent = (self.count + index) >> 1
         while parent > 0:
-            if self.leaves[index] > self.leaves[self.node[parent]]:
+            if self.key(self.leaves[index], self.leaves[self.node[parent]]):
                 # 说明leaves[index]是败者,所以对它的父节点进行赋值,交换后index变成了优胜者
                 self.node[parent], index = index, self.node[parent]
             parent >>= 1
@@ -23,10 +24,10 @@ class LoserTree:
         while True:
             min_pos = self.node[0]
             min_value = self.leaves[min_pos]
-            if min_value == self.max_value:
+            if min_value == self.extreme_value:
                 break
             print(min_value)
-            self.leaves[min_pos] = self.max_value
+            self.leaves[min_pos] = self.extreme_value
             self.adjust(min_pos)
 
 
