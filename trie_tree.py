f2976bfbcdbd4fddfd4426b09a6f94fae3f68a99
neos <akatsuki10086@gmail.com>
2023-06-09 01:31:46 +0800

    Update trie_tree.py

--- trie_tree.py
+++ trie_tree.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, data, flag, left=None, sibling=None):
+    def __init__(self, data, flag=False, left=None, sibling=None):
         self.data = data
         self.flag = flag  # flag可以记录访问次数
         self.left = left
@@ -8,13 +8,20 @@ class Node:
 
 class TrieTree:  # 孩子-兄弟链表存储
     def __init__(self):
-        self.__root = Node('#', False)
+        self._index = -1
+        self._hot = None
+        self.__root = Node('#')
 
     def search(self, word):
+        """
+        没找到情况:
+        1. trie某一层(包括叶子结点后面的一层)没有字符与word对应
+        2. 匹配完word,但flag=False即word仅为某个单词前缀
+        """
         root = self.__root
         for index, alpha in enumerate(word):
-            self._hot = root  # 指向失败层的父节点
-            self._index = index  # 指向匹配失败的位置
+            self._hot = root  # 指向遍历层的父节点
+            self._index = index  # 指向匹配字符位置
             root = root.left
             while root:
                 if root.data == alpha:
@@ -37,13 +44,12 @@ class TrieTree:  # 孩子-兄弟链表存储
             node = Node(word[self._index], False, sibling=self._hot.left)
             self._hot.left = node
             for alpha in word[self._index + 1:]:
-                son = Node(alpha, False)
-                node.left = son
-                node = son
+                node.left = Node(alpha)
+                node = node.left
             node.flag = True
         return True
 
-    def delete(self, word):  # 直接将单词的标志位置为False即可
+    def delete(self, word):  # 简单将单词的标志位置为False(严格点说需要将没用的树枝都删掉)
         if self.search(word):
             siblings = self._hot.left
             while siblings:
@@ -96,4 +102,6 @@ if __name__ == '__main__':
     for word in ['in', 'init', 'ink', 'jack', 'jar', 'john', 'just', 'june', 'junk']:
         trie.insert(word)
     print(trie.delete('john'))
+    print(trie.delete('ink'))
+    print(trie.search("ini"))
     trie.show()
