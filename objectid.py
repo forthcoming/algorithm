0b18bac8b2552b53abd8cc8ab4c2011ca45f85d8
neos <akatsuki10086@gmail.com>
2023-06-19 20:53:34 +0800

    Update objectid.py

--- objectid.py
+++ objectid.py
@@ -1,20 +1,32 @@
-import binascii,calendar,datetime,hashlib,os,random,socket,struct,threading,time
+import binascii
+import calendar
+import datetime
+import hashlib
+import os
+import random
+import socket
+import struct
+import threading
+import time
 
 '''
 代码改编自pymongo的bson.objectid. from bson import objectid
 mongodb的每一条插入语句都会包含if "_id" not in document:document["_id"] = ObjectId()语句
 '''
+
+
 def _machine_bytes():
     machine_hash = hashlib.md5()
     machine_hash.update(socket.gethostname().encode())
     return machine_hash.digest()[0:3]
+
 
 class ObjectId:
     # 此处的代码会在类加载时执行一次,类实例化时不再执行
     _inc = random.randint(0, 0xFFFFFF)
     _inc_lock = threading.Lock()
     _machine_bytes = _machine_bytes()
-    __slots__ = ('__id')
+    __slots__ = '__id'
 
     def __init__(self, oid=None):
         """
@@ -30,11 +42,11 @@ class ObjectId:
         oid can also be class:str of 24 hex digits:
         ObjectId('0123456789ab0123456789ab') # 0123456789ab0123456789ab
         """
-        if oid is None: # Generate a new value for this ObjectId.
+        if oid is None:  # Generate a new value for this ObjectId.
             oid = struct.pack(">i", int(time.time()))  # >: big-endian   i: int 
             oid += ObjectId._machine_bytes
             oid += struct.pack(">H", os.getpid() & 0xFFFF)  # H: unsigned short
-            with ObjectId._inc_lock:              # 确保相同进程的同一秒产生的ID也是不同的,前提是相同进程同一秒产生的ID不能超过2^24
+            with ObjectId._inc_lock:  # 确保相同进程的同一秒产生的ID也是不同的,前提是相同进程同一秒产生的ID不能超过2^24
                 oid += struct.pack(">i", ObjectId._inc)[1:4]  # _inc只有3byte长度so高位会被填充成0
                 ObjectId._inc = (ObjectId._inc + 1) & 0xFFFFFF
             self.__id = oid
@@ -61,9 +73,9 @@ class ObjectId:
         generation_time will be converted to UTC. Naive datetime instances will be treated as though they already contain UTC.
         This method is useful for doing range queries on a field containing class:ObjectId instances.
         An example using this helper to get documents where _id was generated before January 1, 2010 would be:
-        >>> gen_time = datetime.datetime(2010, 1, 1)
-        >>> dummy_id = ObjectId.from_datetime(gen_time)
-        >>> result = collection.find({"_id": {"$lt": dummy_id}})
+        # gen_time = datetime.datetime(2010, 1, 1)
+        # dummy_id = ObjectId.from_datetime(gen_time)
+        # result = collection.find({"_id": {"$lt": dummy_id}})
         generation_time: 
             class:datetime.datetime to be used as the generation time for the resulting ObjectId.
         """
@@ -77,7 +89,8 @@ class ObjectId:
         return datetime.datetime.fromtimestamp(timestamp)
 
     def __str__(self):
-        return binascii.hexlify(self.__id).decode() # 将byte类型转换成byte十六进制类型,再转换成str类型,eg: binascii.hexlify(b'aA').decode()输出6141
+        return binascii.hexlify(
+            self.__id).decode()  # 将byte类型转换成byte十六进制类型,再转换成str类型,eg: binascii.hexlify(b'aA').decode()输出6141
 
     def __hash__(self):  # Get a hash value for this class:ObjectId.
         return hash(self.__id)
