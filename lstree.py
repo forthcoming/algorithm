0c6d31f85928c8416ec3c1d0183b298d81213897
neos <akatsuki10086@gmail.com>
2023-07-06 22:12:03 +0800

    update

--- lstree.py
+++ lstree.py
@@ -1,8 +1,5 @@
-from collections import deque
-
-
 class Node:
-    def __init__(self, data, left=None, right=None):
+    def __init__(self, data=-1, left=None, right=None):
         self.data = data
         self.left = left
         self.right = right
@@ -10,70 +7,64 @@ class Node:
 
 class Hamming:
     def __init__(self, depth=64):
-        self.__root = Node('#')
+        self.__root = Node(-1)
         self.__depth = depth  # signature's binary digits and binary_tree's depth(not including root node)
 
     def insert(self, signature, _id):
         root = self.__root
-        for index in range(self.__depth):  # the nodes generateed from low to high
+        for _ in range(self.__depth):  # the nodes generated from low to high
             if signature & 1:  # left-child:1,right-child:0
                 if not root.left:
-                    root.left = Node(True)
+                    root.left = Node()
                 root = root.left
             else:
                 if not root.right:
-                    root.right = Node(True)
+                    root.right = Node()
                 root = root.right
             signature >>= 1
         root.data = _id
 
-    def BFS(self):
-        count = 0  # node numbers
-        if self.__root:
-            queue = deque((self.__root,))
-            while queue:
-                node = queue.popleft()
-                count += 1
-                print(node)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-        return count
-
     def find(self, signature, dis):
-        root = self.__root
         result = []
 
-        def _find(root, signature, dis, depth):
-            if dis >= 0:
+        def _find(root, _signature, _dis, depth):
+            if _dis >= 0:
                 if depth == self.__depth:
-                    print(root)
+                    print(root.data)
                     result.append(root)
                     return
-                bit = signature & 1
-                signature >>= 1
+                bit = _signature & 1
+                _signature >>= 1
                 if bit:
                     if root.left:
-                        _find(root.left, signature, dis, depth + 1)
+                        _find(root.left, _signature, _dis, depth + 1)
                     if root.right:
-                        _find(root.right, signature, dis - 1, depth + 1)
+                        _find(root.right, _signature, _dis - 1, depth + 1)
                 else:
                     if root.left:
-                        _find(root.left, signature, dis - 1, depth + 1)
+                        _find(root.left, _signature, _dis - 1, depth + 1)
                     if root.right:
-                        _find(root.right, signature, dis, depth + 1)
+                        _find(root.right, _signature, _dis, depth + 1)
 
-        _find(root, signature, dis, 0)
+        _find(self.__root, signature, dis, 0)
         return result
-
-    def delete(self):
-        pass
 
 
 if __name__ == '__main__':
     lsh = Hamming(4)
     signatures = [0b0011, 0b1001, 0b1100, 0b0111]
+    """
+                  #
+              /      \
+              1       0
+            /   \      \
+           1     0      0
+          / \     \     /
+         1   0     0   1
+          \   \    /  /
+           0   0  1  1 
+
+    """
     for index, each in enumerate(signatures):
         lsh.insert(each, index)
-    matched = lsh.find(0b1001, 2)
+    lsh.find(0b1001, 2)
--- minhash.py
+++ minhash.py
@@ -16,39 +16,39 @@ def get_minhash(shingles, n_hashes):
 
 def get_band_hashes(signature, band_size):
     band_hashes = []
-    for i in range(len(signature)):
-        if i % band_size == 0:
-            if i > 0:
-                band_hashes.append(band_hash)
+    band_hash = 0
+    for i, each in enumerate(signature):
+        if i and i % band_size == 0:
+            band_hashes.append(band_hash)
             band_hash = 0
-        band_hash += hash(signature[i])
+        band_hash += hash(each)
     return band_hashes
 
 
-def get_similar_docs(docs, n_hashes=400, band_size=7, shingle_size=3, collectIndexes=True):
+def get_similar_docs(documents, n_hashes=400, band_size=7, shingle_size=3, collect_indexes=True):
     hash_bands = {}
-    docNum = 0
-    for doc in docs:
+    doc_num = 0
+    for doc in documents:
         shingles = {doc[i:i + shingle_size] for i in range(len(doc) - shingle_size + 1)}
         signature = get_minhash(shingles, n_hashes)
         band_hashes = get_band_hashes(signature, band_size)
-        docMember = docNum if collectIndexes else doc
+        doc_member = doc_num if collect_indexes else doc
         for i in range(len(band_hashes)):
             if i not in hash_bands:
                 hash_bands[i] = {}
             if band_hashes[i] not in hash_bands[i]:
-                hash_bands[i][band_hashes[i]] = [docMember]
+                hash_bands[i][band_hashes[i]] = [doc_member]
             else:
-                hash_bands[i][band_hashes[i]].append(docMember)
-        docNum += 1
+                hash_bands[i][band_hashes[i]].append(doc_member)
+        doc_num += 1
 
-    similar_docs = set()
+    similar_documents = set()
     for i in hash_bands:
         for hash_num in hash_bands[i]:
             if len(hash_bands[i][hash_num]) > 1:
                 for pair in itertools.combinations(hash_bands[i][hash_num], r=2):
-                    similar_docs.add(pair)
-    return similar_docs
+                    similar_documents.add(pair)
+    return similar_documents
 
 
 if __name__ == '__main__':
