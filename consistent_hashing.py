13b7b095c431480cc04c8faa9f5a4bc7de955b29
neos <akatsuki10086@gmail.com>
2023-07-08 12:50:30 +0800

    Update consistent_hashing.py

--- consistent_hashing.py
+++ consistent_hashing.py
@@ -3,8 +3,7 @@ from hashlib import md5
 from struct import unpack
 
 
-# Implements consistent hashing that can be used when the number of server nodes can increase or decrease.
-class HashRing:
+class ConsistentHashing:  # can be used when the number of server nodes can increase or decrease.
     def __init__(self, v_node_num=1):
         self.ring = []  # sorted list,只存储节点哈希值
         self.storage = {}  # 节点哈希值与存储元素的映射关系
@@ -18,7 +17,7 @@ class HashRing:
 
     def add_node(self, node):
         for index in range(self.v_node_num):
-            hash_value = HashRing._hash('{}#{}'.format(node, index))
+            hash_value = ConsistentHashing._hash('{}#{}'.format(node, index))
             if hash_value not in self.storage:  # 小概率事件,但也要避免,保证环里面的hash_value唯一
                 self.storage[hash_value] = []
                 if self.node_num:
@@ -35,7 +34,7 @@ class HashRing:
     def add_items(self, items):
         if self.node_num:
             for item in items:
-                hash_value = HashRing._hash(item)
+                hash_value = ConsistentHashing._hash(item)
                 pos = bisect_left(self.ring, hash_value) % self.node_num  # 注意这里跟bisect_right的区别
                 self.storage[self.ring[pos]].append(item)
 
@@ -44,7 +43,7 @@ class HashRing:
 
 
 if __name__ == '__main__':
-    ring = HashRing(2)
+    ring = ConsistentHashing(2)
     for node in range(4):
         ring.add_node(node)
     ring.add_items(map(str, range(100)))
