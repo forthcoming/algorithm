2130675f5424aa027068a775742999088798b68b
neos <akatsuki10086@gmail.com>
2023-07-12 12:53:58 +0800

    Update geohash.py

--- geohash.py
+++ geohash.py
@@ -1,59 +1,10 @@
 import math
 
-__base32 = '0123456789bcdefghjkmnpqrstuvwxyz'
-__mapping = {letter: index for index, letter in enumerate(__base32)}
 
-
-def geo_encode(longitude, latitude, geo_length=11):
-    assert -180 <= longitude <= 180 and -85.05112878 <= latitude <= 85.05112878
-    lon_interval, lat_interval = (-180.0, 180.0), (-85.05112878, 85.05112878)  # 与redis保持一致
-    geohash = 0
-    _geohash = []
-    even = True
-    for i in range(geo_length):
-        for j in [16, 8, 4, 2, 1]:
-            if even:
-                mid = (lon_interval[0] + lon_interval[1]) / 2  # 偶数位放经度
-                if longitude > mid:
-                    geohash |= j
-                    lon_interval = (mid, lon_interval[1])
-                else:
-                    lon_interval = (lon_interval[0], mid)
-            else:
-                mid = (lat_interval[0] + lat_interval[1]) / 2
-                if latitude > mid:
-                    geohash |= j
-                    lat_interval = (mid, lat_interval[1])
-                else:
-                    lat_interval = (lat_interval[0], mid)
-            even = not even
-        _geohash.append(__base32[geohash])
-        geohash = 0
-    return ''.join(_geohash)  # 字符串越长,表示的范围越精确,字符串相似表示距离相近,可以利用字符串的前缀匹配来查询附近信息
-
-
-def geo_decode(geohash):
-    lon_interval, lat_interval = (-180.0, 180.0), (-90.0, 90.0)
-    even = True
-    for letter in geohash:
-        index = __mapping[letter]
-        for mask in [16, 8, 4, 2, 1]:
-            if even:
-                if index & mask:
-                    lon_interval = ((lon_interval[0] + lon_interval[1]) / 2, lon_interval[1])
-                else:
-                    lon_interval = (lon_interval[0], (lon_interval[0] + lon_interval[1]) / 2)
-            else:
-                if index & mask:
-                    lat_interval = ((lat_interval[0] + lat_interval[1]) / 2, lat_interval[1])
-                else:
-                    lat_interval = (lat_interval[0], (lat_interval[0] + lat_interval[1]) / 2)
-            even = not even
-    return (lon_interval[0] + lon_interval[1]) / 2, (lat_interval[0] + lat_interval[1]) / 2
-
-
-class GEO:
+class GeoHash:
     _earth_radius = 6372797.560856
+    base32 = "0123456789bcdefghjkmnpqrstuvwxyz"
+    mapping = {letter: index for index, letter in enumerate(base32)}
 
     def __init__(self, step=30):
         assert 0 < step <= 32
@@ -65,10 +16,10 @@ class GEO:
         lat_offset = latitude / 180 + .5
         lon_offset *= self.scale
         lat_offset *= self.scale
-        return __class__.interleave64(int(lat_offset), int(lon_offset))
+        return GeoHash.interleave64(int(lat_offset), int(lon_offset))
 
     def decode(self, geohash):
-        lon_offset, lat_offset = __class__.deinterleave64(geohash)
+        lon_offset, lat_offset = GeoHash.de_interleave64(geohash)
         longitude = 360 * lon_offset / self.scale - 180
         latitude = 180 * lat_offset / self.scale - 90
         return longitude, latitude
@@ -82,7 +33,7 @@ class GEO:
         lat2 *= percent
         u = math.sin((lat2 - lat1) / 2)
         v = math.sin((lon2 - lon1) / 2)
-        return round(2 * __class__._earth_radius * math.asin((u * u + math.cos(lat1) * math.cos(lat2) * v * v) ** .5))
+        return round(2 * GeoHash._earth_radius * math.asin((u * u + math.cos(lat1) * math.cos(lat2) * v * v) ** .5))
 
     @staticmethod
     def interleave64(x, y):
@@ -107,7 +58,7 @@ class GEO:
         return x | (y << 1)
 
     @staticmethod
-    def deinterleave64(interleaved):
+    def de_interleave64(interleaved):
         B = [0x3333333333333333, 0x0F0F0F0F0F0F0F0F, 0x00FF00FF00FF00FF, 0x0000FFFF0000FFFF, 0x00000000FFFFFFFF]
         S = [1, 2, 4, 8, 16]
 
@@ -130,3 +81,63 @@ class GEO:
         y = (y | (y >> S[4])) & B[4]
 
         return y, x
+
+    @staticmethod
+    def encode_lower_version(longitude, latitude, geo_length=11):
+        assert -180 <= longitude <= 180 and -85.05112878 <= latitude <= 85.05112878
+        lon_interval, lat_interval = (-180.0, 180.0), (-85.05112878, 85.05112878)  # 与redis保持一致
+        geohash = 0
+        _geohash = []
+        even = True
+        for i in range(geo_length):
+            for j in [16, 8, 4, 2, 1]:
+                if even:
+                    mid = (lon_interval[0] + lon_interval[1]) / 2  # 偶数位放经度
+                    if longitude > mid:
+                        geohash |= j
+                        lon_interval = (mid, lon_interval[1])
+                    else:
+                        lon_interval = (lon_interval[0], mid)
+                else:
+                    mid = (lat_interval[0] + lat_interval[1]) / 2
+                    if latitude > mid:
+                        geohash |= j
+                        lat_interval = (mid, lat_interval[1])
+                    else:
+                        lat_interval = (lat_interval[0], mid)
+                even = not even
+            _geohash.append(GeoHash.base32[geohash])
+            geohash = 0
+        return ''.join(_geohash)  # 字符串越长,表示的范围越精确,字符串相似表示距离相近,可以利用字符串的前缀匹配来查询附近信息
+
+    @staticmethod
+    def decode_lower_version(geohash):
+        lon_interval, lat_interval = (-180.0, 180.0), (-90.0, 90.0)
+        even = True
+        for letter in geohash:
+            index = GeoHash.mapping[letter]
+            for mask in [16, 8, 4, 2, 1]:
+                if even:
+                    if index & mask:
+                        lon_interval = ((lon_interval[0] + lon_interval[1]) / 2, lon_interval[1])
+                    else:
+                        lon_interval = (lon_interval[0], (lon_interval[0] + lon_interval[1]) / 2)
+                else:
+                    if index & mask:
+                        lat_interval = ((lat_interval[0] + lat_interval[1]) / 2, lat_interval[1])
+                    else:
+                        lat_interval = (lat_interval[0], (lat_interval[0] + lat_interval[1]) / 2)
+                even = not even
+        return (lon_interval[0] + lon_interval[1]) / 2, (lat_interval[0] + lat_interval[1]) / 2
+
+
+if __name__ == "__main__":
+    longitude, latitude = 123, 76
+    geo = GeoHash()
+    encoded = geo.encode(longitude, latitude)
+    decoded = geo.decode(encoded)
+    print(encoded, decoded)
+
+    encoded = geo.encode_lower_version(longitude, latitude)
+    decoded = geo.decode_lower_version(encoded)
+    print(encoded, decoded)
